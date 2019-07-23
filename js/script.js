0724c9aa1c120c7a2333c27d2c6250f028ea1981
Arbias Krasniqi <k.arbias@live.com>
2019-07-23 13:24:22 +0200

    Added a loading gif

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+$(window).on("load", function(){
+
+	$(".loader .inner").fadeOut(500, function(){
+		$(".loader").fadeOut(750);
+	});
+
+	$(".items").isotope({
+ 		filter: '*',
+ 		animationOptions:{
+ 			duration:1500,
+ 			easing: 'linear',
+ 			queue: false
+ 		}
+ 	});
+ 		
+})
+
 $(document).ready(function() {
 
 	$('#slides').superslides({
@@ -71,16 +88,6 @@ $(document).ready(function() {
  		});
 
  		$("[data-fancybox]").fancybox();	
-
- 		$(".items").isotope({
- 			filter: '*',
- 			animationOptions:{
- 				duration:1500,
- 				easing: 'linear',
- 				queue: false
- 			}
- 		});
- 		
 
  		$("#filters a").click(function(){
 
